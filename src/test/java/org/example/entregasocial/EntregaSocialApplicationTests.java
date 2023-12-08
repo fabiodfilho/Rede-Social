364e03f5235e71fc146875bd9ef9cb983e03b1ed
fabiodfilho <fabiodfilho.dev@gmail.com>
2023-12-08 16:13:29 -0300

    initial commit

---
+++ src/test/java/org/example/entregasocial/EntregaSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.entregasocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
